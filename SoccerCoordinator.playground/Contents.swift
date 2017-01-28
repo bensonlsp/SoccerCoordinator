7b70c9b6ae028fda149aed5c2c6797833fd8c12a
Benson Lo <bensonlo@Bensonde-MacBook-Air.local>
2017-01-28 17:59:23 +0800

    Rewrite the collection to store players to fine tune the programme to avoid type problem.

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,85 +1,67 @@
 // Project 1 of TreeHouse TechDegree
+// import GameplayKit
 import GameplayKit
 
-// The collection, a dictionary, to contain all players.
-// The key of the dictionary is the player name in String type
-// The value of the dictionary is an array of the first value being the height in inches, the second value being the soccer experience, the third value being the guardian name, all in String type.
-var players: [String: [String]] = [
-    "Joe Smith": ["42", "YES", "Jim and Jan Smith"],
-    "Jill Tanner": ["36", "YES", "Clara Tanner"],
-    "BIll Bon": ["43", "YES", "Sara and Jeny Bon"],
-    "Eva Gordon": ["45", "NO", "Wendy and Mike Gordon"],
-    "Matt Gill": ["40", "NO", "Charles an Sylvia Gill"],
-    "Kimmy Stein": ["41", "NO", "BIll and Hillary Stein"],
-    "Sammy Adams": ["45", "NO", "Jeff Adams"],
-    "Karl Saygan": ["42", "YES", "Heather Bledsoe"],
-    "Suzane Greenberg": ["44", "YES", "Henrietta Dumas"],
-    "Sal Dali": ["41", "NO", "Gala Dali"],
-    "Joe Kavalier": ["39", "NO", "Sam and Elaine Kavalier"],
-    "Ben Finkelstein": ["44", "NO", "Aaron and Jill Finkelstein"],
-    "Diego Soto": ["41", "YES", "Robin and Sarika Soto"],
-    "Chloe Alaska": ["47", "NO", "David and Jamie Alaska"],
-    "Arnold Willis": ["43", "NO", "Chaire Willis"],
-    "Philip Helm": ["44", "YES", "Thomas Helm and Eva Jones"],
-    "Les Clay": ["42", "YES", "Wynonna Brown"],
-    "Herschel Krustofski": ["45", "YES", "Hyman and Rachel Krutoski"]
-]
+// The array to contain all players is called players. Each player will be stored as a dictionary.
+let player0: [String: Any] = ["name": "Joe Smith", "height": 42.0, "experience": true, "guardian": "Jim and Jan Smith"]
+let player1: [String: Any] = ["name": "Jill Tanner", "height": 36.0, "experience": true, "guardian": "Clara Tanner"]
+let player2: [String: Any] = ["name": "BIll Bon", "height": 43.0, "experience": true, "guardian": "Sara and Jeny Bon"]
+let player3: [String: Any] = ["name": "Eva Gordon", "height": 45.0, "experience": false, "guardian": "Wendy and Mike Gordon"]
 
-// The collection, also a dictionary, to store players in different team
-var teamSharks: [String: [String]] = [:]
-var teamDragon: [String: [String]] = [:]
-var teamRaptors: [String: [String]] = [:]
+let player4: [String: Any] = ["name": "Matt Gill", "height": 40.0, "experience": false, "guardian": "Charles an Sylvia Gill"]
+let player5: [String: Any] = ["name": "Kimmy Stein", "height": 41.0, "experience": false, "guardian":  "BIll and Hillary Stein"]
+let player6: [String: Any] = ["name": "Sammy Adams", "height": 45.0, "experience": false, "guardian": "Jeff Adams"]
+let player7: [String: Any] = ["name": "Karl Saygan", "height": 42.0, "experience": true, "guardian": "Heather Bledsoe"]
 
-// The function to read the height, experience and guardian information from a collection of players according to the player name.
-// The height, experience and guardian information will be returned as a tuple.
-func playerInformation(ofPlayerName name: String, inPlayers players: [String: [String]])
-    -> (height: String, experience: String, guardian: String) {
-    if let playerInformation = players[name] {
-        return (playerInformation[0], playerInformation[1], playerInformation[2])
-    } else {
-        return ("N/A", "N/A", "N/A")
-    }
-}
+let player8: [String: Any] = ["name": "Suzane Greenberg", "height": 44.0, "experience": true, "guardian": "Henrietta Dumas"]
+let player9: [String: Any] = ["name": "Sal Dali", "height": 41.0, "experience": false, "guardian": "Gala Dali"]
+let player10: [String: Any] = ["name": "Joe Kavalier", "height": 39.0, "experience": false, "guardian": "Sam and Elaine Kavalier"]
+let player11: [String: Any] = ["name": "Ben Finkelstein", "height": 44.0, "experience": false, "guardian": "Aaron and Jill Finkelstein"]
 
-// The function to calculate average height of players in a collection
-func averageHeight(players: [String: [String]]) -> Double {
+let player12: [String: Any] = ["name": "Diego Soto", "height": 41.0, "experience": true, "guardian": "Robin and Sarika Soto"]
+let player13: [String: Any] = ["name": "Chloe Alaska", "height": 47.0, "experience": false, "guardian": "David and Jamie Alaska"]
+let player14: [String: Any] = ["name": "Arnold Willis", "height": 43.0, "experience": false, "guardian": "Chaire Willis"]
+let player15: [String: Any] = ["name": "Philip Helm", "height": 44.0, "experience": true, "guardian": "Thomas Helm and Eva Jones"]
+
+let player16: [String: Any] = ["name": "Les Clay", "height": 42.0, "experience": true, "guardian": "Wynonna Brown"]
+let player17: [String: Any] = ["name": "Herschel Krustofski", "height": 45.0, "experience": true, "guardian": "Hyman and Rachel Krutoski"]
+
+var players: [[String:Any]] = [player0, player1, player2, player3, player4, player5, player6, player7, player8, player9,
+    player10, player11, player12, player13, player14, player15, player16, player17]
+
+// The arrays to store players in different teams
+var teamSharks: [[String: Any]] = []
+var teamDragons: [[String: Any]] = []
+var teamRaptors: [[String: Any]] = []
+
+// The function to calculate average height of players in an array
+func averageHeight(players:  [[String: Any]]) -> Double {
     var heightTotal: Double = 0
 
     for player in players {
-        if let height = Double(playerInformation(ofPlayerName: player.key, inPlayers: players).height) {
-            heightTotal += height
+        if let playerHeight = player["height"] {
+            heightTotal += playerHeight as! Double
         }
     }
     
     return heightTotal/Double(players.count)
 }
 
-// Function to assign a player to a team usign the player name as the key. The value of the player will be gotten from the players collection.
-func assignPlayer(ofPlayerName player: String, intoTeam team: String) {
-    switch team {
-        case "Dragon": teamDragon[player] = [playerInformation(ofPlayerName: player, inPlayers: players).height, playerInformation(ofPlayerName: player, inPlayers: players).experience, playerInformation(ofPlayerName: player, inPlayers: players).guardian]
-        case "Sharks": teamSharks[player] = [playerInformation(ofPlayerName: player, inPlayers: players).height, playerInformation(ofPlayerName: player, inPlayers: players).experience, playerInformation(ofPlayerName: player, inPlayers: players).guardian]
-        case "Raptors": teamRaptors[player] = [playerInformation(ofPlayerName: player, inPlayers: players).height, playerInformation(ofPlayerName: player, inPlayers: players).experience, playerInformation(ofPlayerName: player, inPlayers: players).guardian]
-        default: print("No such team. Error.")
-    }
-}
-
 // The function to sort players into the three teams by their experience and height
-func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
+func sortPlayersIntoThreeTeams(fromPlayers players: [[String: Any]]) {
     // The two arrays below will used as temporary place to divide players into two groups: with experience or without experience
-    // Only the name of the player will be stored in the two arrays
-    var playersWithExperience: [String] = []
-    var playersWithOutExperience: [String] = []
+    var playersWithExperience: [[String: Any]] = []
+    var playersWithOutExperience: [[String: Any]] = []
     // The variable will be used to indicate whether all teams' average heights are within 1.5 inches of each other
     // The initial value will be false
     // If the criteria could not be met, the assignment logic will run again until the criteria could be met.
     var heightRequirementOK = false
     
     // Players will be tried to be assigned into the three teams so that they will have even experienced player.
-    // And, all teams' average heights are within 1.5 inches of each other, otherwise, the assignment logic will loop again.
+    // And, all teams' average heights will be ensured to be within 1.5 inches of each other, otherwise, the assignment logic will loop again.
     while heightRequirementOK == false {
-        // To remove any previous data in the collections to be used to prevent from unexpected error
-        teamDragon.removeAll()
+        // To remove and reset any previous data in the collections to be used to prevent from unexpected error
+        teamDragons.removeAll()
         teamSharks.removeAll()
         teamRaptors.removeAll()
         playersWithExperience.removeAll()
@@ -87,14 +69,16 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
         
         // Divide players into two groups: with experience or without experience
         for player in players {
-            switch playerInformation(ofPlayerName: player.key, inPlayers: players).experience {
-                case "YES": playersWithExperience.append(player.key)
-                case "NO": playersWithOutExperience.append(player.key)
-                default: print("Error! No information about his/her soccer found. Please check again the players collection.")
+            if (player["experience"] as! Bool) == true {
+                playersWithExperience.append(player)
+            } else if (player["experience"] as! Bool) == false {
+                playersWithOutExperience.append(player)
+            } else {
+                print("Unexpected erro!")
             }
         }
         
-        // Variables used to store random number to assign player into team randomly
+        // Variables used to store random number for assignment of player into the three teams randomly
         var randomTeam1: Int = 1
         var randomTeam2: Int = 2
         var randomTeam3: Int = 3
@@ -110,10 +94,10 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
             randomTeam1 = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
             
             switch randomTeam1 {
-                case 0: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer1], intoTeam: "Dragon")
-                case 1: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer1], intoTeam: "Sharks")
-                case 2: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer1], intoTeam: "Raptors")
-                default: print("Unexpected error")
+            case 0: teamDragons.append(playersWithExperience[randomPlayer1])
+            case 1: teamSharks.append(playersWithExperience[randomPlayer1])
+            case 2: teamRaptors.append(playersWithExperience[randomPlayer1])
+            default: print("Unexpected error")
             }
             
             playersWithExperience.remove(at: randomPlayer1)
@@ -128,10 +112,10 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
                 }
             
                 switch randomTeam2 {
-                    case 0: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer2], intoTeam: "Dragon")
-                    case 1: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer2], intoTeam: "Sharks")
-                    case 2: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer2], intoTeam: "Raptors")
-                    default: print("Unexpected error")
+                case 0: teamDragons.append(playersWithExperience[randomPlayer2])
+                case 1: teamSharks.append(playersWithExperience[randomPlayer2])
+                case 2: teamRaptors.append(playersWithExperience[randomPlayer2])
+                default: print("Unexpected error")
                 }
             
                 playersWithExperience.remove(at: randomPlayer2)
@@ -147,10 +131,10 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
                 }
             
                 switch randomTeam3 {
-                    case 0: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer3], intoTeam: "Dragon")
-                    case 1: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer3], intoTeam: "Sharks")
-                    case 2: assignPlayer(ofPlayerName: playersWithExperience[randomPlayer3], intoTeam: "Raptors")
-                    default: print("Unexpected error")
+                case 0: teamDragons.append(playersWithExperience[randomPlayer3])
+                case 1: teamSharks.append(playersWithExperience[randomPlayer3])
+                case 2: teamRaptors.append(playersWithExperience[randomPlayer3])
+                default: print("Unexpected error")
                 }
             
                 playersWithExperience.remove(at: randomPlayer3)
@@ -168,10 +152,10 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
             randomTeam1 = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
             
             switch randomTeam1 {
-                case 0: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer1], intoTeam: "Dragon")
-                case 1: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer1], intoTeam: "Sharks")
-                case 2: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer1], intoTeam: "Raptors")
-                default: print("Unexpected error")
+            case 0: teamDragons.append(playersWithOutExperience[randomPlayer1])
+            case 1: teamSharks.append(playersWithOutExperience[randomPlayer1])
+            case 2: teamRaptors.append(playersWithOutExperience[randomPlayer1])
+            default: print("Unexpected error")
             }
             
             playersWithOutExperience.remove(at: randomPlayer1)
@@ -186,10 +170,10 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
                 }
         
                 switch randomTeam2 {
-                    case 0: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer2], intoTeam: "Dragon")
-                    case 1: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer2], intoTeam: "Sharks")
-                    case 2: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer2], intoTeam: "Raptors")
-                    default: print("Unexpected error")
+                case 0: teamDragons.append(playersWithOutExperience[randomPlayer2])
+                case 1: teamSharks.append(playersWithOutExperience[randomPlayer2])
+                case 2: teamRaptors.append(playersWithOutExperience[randomPlayer2])
+                default: print("Unexpected error")
                 }
             
                 playersWithOutExperience.remove(at: randomPlayer2)
@@ -205,12 +189,12 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
                 }
             
                 switch randomTeam3 {
-                    case 0: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer3], intoTeam: "Dragon")
-                    case 1: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer3], intoTeam: "Sharks")
-                    case 2: assignPlayer(ofPlayerName: playersWithOutExperience[randomPlayer3], intoTeam: "Raptors")
-                    default: print("Unexpected error")
+                case 0: teamDragons.append(playersWithOutExperience[randomPlayer3])
+                case 1: teamSharks.append(playersWithOutExperience[randomPlayer3])
+                case 2: teamRaptors.append(playersWithOutExperience[randomPlayer3])
+                default: print("Unexpected error")
                 }
-            
+                
                 playersWithOutExperience.remove(at: randomPlayer3)
             }
         }
@@ -220,9 +204,9 @@ func sortPlayersIntoThreeTeams(fromPlayers players: [String: [String]]) {
         // Otherwise, players will be picked into the three teams again by random assignment until the criteria could be met.
         // As such, the criteria will be met by trial and error.
         // For each time of assignment, the three teams will have even experienced players.
-        if (averageHeight(players: teamDragon)-averageHeight(players: teamSharks)<1.5 &&
-            averageHeight(players: teamSharks)-averageHeight(players: teamRaptors)<1.5 &&
-            averageHeight(players: teamRaptors)-averageHeight(players: teamDragon)<1.5) {
+        if (abs(averageHeight(players: teamDragons)-averageHeight(players: teamSharks))<1.5 &&
+            abs(averageHeight(players: teamSharks)-averageHeight(players: teamRaptors))<1.5 &&
+            abs(averageHeight(players: teamRaptors)-averageHeight(players: teamDragons))<1.5) {
             heightRequirementOK = true
         }
     }
@@ -235,14 +219,20 @@ sortPlayersIntoThreeTeams(fromPlayers: players)
 var letters: [String] = []
 
 // To produce and save the letter into the letters collection
-for player in teamDragon {
-    letters.append("Dear \(playerInformation(ofPlayerName: player.key, inPlayers: teamDragon).guardian),\nI would like to inform you that your child, named \(player.key), has been put into the Team Dragon. Please arrange your child to attend the training on March 17, 1pm. Thank you very much.\nFrom: TreeHouse Soccer.")
+for player in teamDragons {
+    let guardian = player["guardian"] as! String
+    let name = player["name"] as! String
+    letters.append("Dear \(guardian),\nI would like to inform you that your child, named \(name), has been put into the Team Dragons. Please arrange your child to attend the training on March 17, 1pm. Thank you very much.\nFrom: TreeHouse Soccer.")
 }
 for player in teamSharks {
-    letters.append("Dear \(playerInformation(ofPlayerName: player.key, inPlayers: teamSharks).guardian),\nI would like to inform you that your child, named \(player.key), has been put into the Team Sharks. Please arrange your child to attend the training on March 17, 3pm. Thank you very much.\nFrom: TreeHouse Soccer.")
+    let guardian = player["guardian"] as! String
+    let name = player["name"] as! String
+    letters.append("Dear \(guardian),\nI would like to inform you that your child, named \(name), has been put into the Team Sharks. Please arrange your child to attend the training on March 17, 3pm. Thank you very much.\nFrom: TreeHouse Soccer.")
 }
 for player in teamRaptors {
-    letters.append("Dear \(playerInformation(ofPlayerName: player.key, inPlayers: teamRaptors).guardian),\nI would like to inform you that your child, named \(player.key), has been put into the Team Raptors. Please arrange your child to attend the training on March 18, 1pm. Thank you very much.\nFrom: TreeHouse Soccer.")
+    let guardian = player["guardian"] as! String
+    let name = player["name"] as! String
+    letters.append("Dear \(guardian),\nI would like to inform you that your child, named \(name), has been put into the Team Raptors. Please arrange your child to attend the training on March 18, 1pm. Thank you very much.\nFrom: TreeHouse Soccer.")
 }
 
 // Print out the letter to the console
@@ -252,7 +242,7 @@ for letter in letters {
 }
 
 // Print out average height of each team
-print("Average height of team Dragon = \(averageHeight(players: teamDragon))")
+print("Average height of team Dragons = \(averageHeight(players: teamDragons))")
 print("Average height of team Sharks = \(averageHeight(players: teamSharks))")
 print("Average height of team Raptors = \(averageHeight(players: teamRaptors))")
 
